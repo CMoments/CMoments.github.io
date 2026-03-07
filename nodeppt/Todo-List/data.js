7fffbfcabfddf065982d989dfb6e3327b65d6768
CMoments <2319317080@qq.com>
2026-03-07 16:55:05 +0800

    Site updated: 2026-03-07 16:55:04

--- nodeppt/Todo-List/data.js
+++ nodeppt/Todo-List/data.js
@@ -11,119 +11,11 @@ function getData() {
         {
             "id": "1",
             "project": "introduction",
-            "text": "Greetings, everyone! \u{1F44B} \n Welcome to our Todo-List for CISCN2026 \u{1F389} \n",
+            "text": "Greetings, everyone! \u{1F44B} \n Welcome to our Todo-List for QEC\u{1F389} \n",
 			"checked": false
 		},
-		{
-		    "id": "2",
-		    "project": "introduction",
-		    "text": "Note \u{1F6A8} \nThis is a static page,which means that your modification only take effect on your browser. \n A backend for our teamwork is on its way.",
-		},
-        {
-            "id": "3",
-            "project": "introduction",
-            "text": "Todo-List功能：\n 你可以使用菜单为任务分配完成人和截止日期。",
-            "assigned": [ "user_2", "user_1", "user_3" ],
-            "due_date": "2033-03-08T21:00:00.000Z"
-        },
-        {
-            "id": "4",
-            "project": "introduction",
-            "text": "Toolbar功能：\n 请在上边栏Toolbar中选择对应学习周的项目=> Learning Week1\u{1F929}",
-        },
-        {
-            "id": "5",
-            "project": "introduction",
-            "parent": "4",
-            "text": "Learning Week1",
-            "checked": true
-        },
-        {
-            "id": "6",
-            "project": "introduction",
-            "parent": "4",
-            "text": "Learning Week2",
-            "checked": false
-        },
-		{
-			"id": "7",
-			"project": "Learning Week1",
-			"parent": "15",
-			"text": "Introduction to Agentic AI\u{1F4D3} \n https://www.bilibili.com/video/BV1DfrdByE2H?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d",
-			"checked": false
-		},
-		{
-			"id": "8",
-			"project": "Learning Week1",
-			"parent": "15",
-			"text": "What is Agentic AI\u{1F4D3} \n https://www.bilibili.com/video/BV1DfrdByE2H?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=2",
-			"checked": false
-		},
-		{
-			"id": "9",
-			"project": "Learning Week1",
-			"parent": "15",
-			"text": "Introduction to ClaudeCode \u{1F4D3} \n https://www.bilibili.com/video/BV19LSaBKE2S?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d",
-			"checked": false
-		},
-		{
-			"id": "10",
-			"project": "Learning Week1",
-			"parent": "15",
-			"text": "What is ClaudeCode \u{1F4D3} \nhttps://www.bilibili.com/video/BV19LSaBKE2S?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=2",
-			"checked": false
-		},
-		{
-			"id": "11",
-			"project": "Learning Week1",
-			"parent": "14",
-			"text": "Introduction to Agent Skills \u{1F4D3} \nhttps://www.bilibili.com/video/BV1qv6eBZErD/?spm_id_from=333.337.search-card.all.click&vd_source=b1e7090f559cd11184bba2cec638592d",
-			"checked": false
-		},
-		{
-			"id": "12",
-			"project": "Learning Week1",
-			"parent": "14",
-			"text": "Why Use Skills-1 \u{1F4D3} \nhttps://www.bilibili.com/video/BV1qv6eBZErD?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=2",
-			"checked": false
-		},
-        {
-        	"id": "13",
-        	"project": "Learning Week1",
-			"parent": "14",
-        	"text": "Why Use Skills-2 \u{1F4D3} \nhttps://www.bilibili.com/video/BV1qv6eBZErD?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=3",
-        	"checked": false
-        },
-		
-		{
-			"id": "15",
-			"project": "Learning Week1",
-			"text": "Agent Skills Background\u{1F4F9} \nRelative Information and Background knowledge",
-			"assigned": [ "user_2", "user_1", "user_3", "user_4"],
-			"due_date": "2026-02-13T00:00:00.000Z",
 
-		},
-		{
-			"id": "16",
-			"project": "Learning Week1",
-			"text": "Agent Skills\u{1F4F9} \n Watch the video and start your journey.",
-			"assigned": [ "user_2", "user_1", "user_3", "user_4"],
-			"due_date": "2026-02-13T00:00:00.000Z",
-		},
-		{
-			"id": "17",
-			"project": "Learning Week2",
-			"text": "Agent Skills\u{1F4F9} \n https://www.bilibili.com/video/BV1qv6eBZErD?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=4",
-			"assigned": [ "user_2", "user_1", "user_3", "user_4"],
-			"due_date": "2026-02-20T00:00:00.000Z",		
-		},
-		{
-			"id": "18",
-			"project": "Learning Week2",
-			"text": "Agent Skills\u{1F4F9} \n https://www.bilibili.com/video/BV1qv6eBZErD?spm_id_from=333.788.videopod.episodes&vd_source=b1e7090f559cd11184bba2cec638592d&p=5",
-			"assigned": [ "user_2", "user_1", "user_3", "user_4"],
-			"due_date": "2026-02-20T00:00:00.000Z",		
-		}
+		
     ];
 
     // data for users
@@ -137,11 +29,8 @@ function getData() {
 
     // data for projects
     const projects = [
-		{ "id": "introduction", "label": "Introduction to CISCN-2026 teamwork proactive" },
-        { "id": "Learning Week1", "label": "Learning Week1\u{1F914}: Agentic AI and Agent Skills Introduction" },
-        { "id": "Learning Week2", "label": "Learning Week2\u{1F929}: SKill and Agent, MCP and Subagent." },
-		{ "id": "Learning Week3", "label": "Learning Week3\u{1F914}: Exploring Prebuilt Skills." },
-        // more project objects
+		{ "id": "introduction", "label": "Introduction to QEC teamwork proactive" },
+  
 		// { "id": "widgets", "label": "Our widgets" },
     ];
 	const priorities = [
